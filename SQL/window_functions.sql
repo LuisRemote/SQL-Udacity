785f3f487bdacad185a669484f8b7ced0b62e12c
LuisRemote <ortizangeles.mx@gmail.com>
2022-07-09 23:39:31 -0500

    window functions update

--- SQL/window_functions.sql
+++ SQL/window_functions.sql
@@ -55,3 +55,35 @@ SELECT id,
        MAX(standard_qty) OVER (PARTITION BY account_id) AS max_std_qty
 FROM orders
 
+
+--=== Quiz 5: Aliases for Multiple Window Functions===--
+-- Now, create and use an alias to shorten the following query (which is different
+-- than the one in Derek's previous video) that has multiple window functions.
+-- Name the alias account_year_window, which is more
+-- descriptive than main_window in the example above.
+SELECT id,
+       account_id,
+       DATE_TRUNC('year',occurred_at) AS year,
+       DENSE_RANK() OVER account_year_window AS dense_rank,
+       total_amt_usd,
+       SUM(total_amt_usd) OVER account_year_window AS sum_total_amt_usd,
+       COUNT(total_amt_usd) OVER account_year_window AS count_total_amt_usd,
+       AVG(total_amt_usd) OVER account_year_window AS avg_total_amt_usd,
+       MIN(total_amt_usd) OVER account_year_window AS min_total_amt_usd,
+       MAX(total_amt_usd) OVER account_year_window AS max_total_amt_usd
+FROM orders
+WINDOW account_year_window AS (PARTITION BY account_id ORDER BY DATE_TRUNC('year',occurred_at))
+
+
+
+--=== Quiz 6: Comparing a Row to Previous Row ===--
+-- In the previous video, Derek outlines how to compare a row to a previous or subsequent row.
+-- This technique can be useful when analyzing time-based events. Imagine you're an analyst at
+-- Parch & Posey and you want to determine how the current order's total revenue ("total"
+-- meaning from sales of all types of paper) compares to the next order's total revenue.
+
+-- Modify Derek's query from the previous video in the SQL Explorer below to perform this
+-- analysis. You'll need to use occurred_at and total_amt_usd in the orders table along with
+-- LEAD to do so. In your query results, there should be four columns: occurred_at,
+-- total_amt_usd, lead and lead_difference
+
