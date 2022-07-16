29e08feb49074a602dd19b7d21eea82321e09727
LuisRemote <ortizangeles.mx@gmail.com>
2022-07-15 22:34:52 -0500

    union quiz added

--- SQL/advance.sql
+++ SQL/advance.sql
@@ -41,3 +41,35 @@ AND w2.occurred_at > w1.occurred_at
 AND w2.occurred_at <= w1.occurred_at + INTERVAL '1 day'
 ORDER BY w1.account_id, w2.occurred_at
 
+--== Quiz 4: Union ===--
+-- Write a query that uses UNION ALL on two instances (and selecting all columns) of
+-- the accounts table. Then inspect the results and answer the subsequent quiz
+SELECT *
+FROM accounts
+UNION ALL
+SELECT *
+FROM accounts
+
+-- Add a WHERE clause to each of the tablese that you unioned in the query above, filtering
+-- the first table where name equals 'Walmart' and filtering the second table where name
+-- equals 'Disney'. Inspect the results then answer the subsequent quiz
+SELECT *
+FROM accounts
+WHERE name = 'Walmart'
+UNION ALL
+SELECT *
+FROM accounts
+WHERE name = 'Disney'
+
+-- Perform the union in your first query, in a CTE, your result should have a count of 2
+-- for each name
+WITH double_accounts AS (
+SELECT *
+FROM accounts
+UNION ALL
+SELECT *
+FROM accounts)
+
+SELECT name, COUNT(name) AS events
+FROM double_accounts
+GROUP BY name
